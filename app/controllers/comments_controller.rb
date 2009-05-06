d70b47370e059a2f3785c95262edbde7abda30b1
paulcc <paulcc.two@gmail.com>
2009-05-06 17:06:54 +0100

    reverted to spree's "require_user" method (and hence avoids deprecated form)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   helper BlogEntriesHelper
 
   # no comments without login first
-  before_filter :require_user_account, :only => [:new,:create] 
+  before_filter :require_user, :only => [:new,:create] 
 
   def new 
     @blog = BlogEntry.find(params[:blog_entry_id])
@@ -28,12 +28,4 @@ class CommentsController < ApplicationController
       render :action => "new"
     end
   end
-
-  private
-  def require_user_account
-    return if logged_in?
-    store_location
-    redirect_to signup_path
-  end
-
 end
